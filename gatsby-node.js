251adbc6759925a66904320562f060c593d3b833
adewale ayomiposi <phil.a@softquestsystems.com>
2020-08-27 23:14:38 +0100

    new update

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,11 +1,11 @@
 //gatsby-node.js
-exports.onCreateWebpackConfig = ({
-    actions,
-  }) => {
-    const { setWebpackConfig } = actions;
-    setWebpackConfig({
-      externals: {
-        jquery: 'jquery', // important: 'Q' capitalized
-      }
-    })
-  }
+// exports.onCreateWebpackConfig = ({
+//     actions,
+//   }) => {
+//     const { setWebpackConfig } = actions;
+//     setWebpackConfig({
+//       externals: {
+//         jquery: 'jquery', // important: 'Q' capitalized
+//       }
+//     })
+//   }
