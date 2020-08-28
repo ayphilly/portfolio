4c32ddf3392bbd9cc76eae7b3ec305799802dba5
adewale ayomiposi <phil.a@softquestsystems.com>
2020-08-28 02:07:29 +0100

    update to layout

--- src/components/about-me.js
+++ src/components/about-me.js
@@ -16,7 +16,7 @@ const AboutPage = () => {
         graphql` query {
             placeholderImage: file(relativePath: {eq: "images/wale1.png"}){
                 childImageSharp {
-                    fluid(fit: COVER, maxWidth: 500, pngQuality: 10){
+                    fluid(maxWidth: 400, pngQuality: 10){
                         ...GatsbyImageSharpFluid
                     }
                 }
