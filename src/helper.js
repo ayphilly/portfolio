d095bb46d57c1b1b320fbba7021f80cbecbbd782
adewale ayomiposi <aphilemon.aa@gmail.com>
2020-08-28 20:34:54 +0100

    update to layout

--- src/helper.js
+++ src/helper.js
@@ -34,6 +34,7 @@ export const barCont = () => {
         linking[i].addEventListener("click", function () {
             const overlay = document.querySelector('.overlay');
             overlay.style.opacity=0;
+            overlay.style.zIndex= 100;
             tlm.reversed()? tlm.play() : tlm.reverse();
         });
     }
